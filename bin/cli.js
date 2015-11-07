2dcc8944fd10b69392c363124671888328af43e3
Bryan Kendall <bryan@bryankendall.com>
2015-11-06 22:11:49 -0800

    apply standard format

--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-var dockerMock = require('../lib');
-var argv = require('minimist')(process.argv.slice(2));
-var port = parseInt(argv.p || argv.port, 10) || 5354;
+var dockerMock = require('../lib')
+var argv = require('minimist')(process.argv.slice(2))
+var port = parseInt(argv.p || argv.port, 10) || 5354
 
-main();
+main()
 
 function main () {
   if (argv.h || argv.help) {
     console.log('Usage: docker-mock [-p=5354]\n' +
       '       docker-mock -h | --help\n\n' +
       'Options:\n' +
-      '   -p, --port  The port to listen on.');
-    return;
+      '   -p, --port  The port to listen on.')
+    return
   }
 
-  dockerMock.listen(port);
-  console.log('docker-mock is listening on port %d...', port);
+  dockerMock.listen(port)
+  console.log('docker-mock is listening on port %d...', port)
 }
--- test/functional/docker.js
+++ test/functional/docker.js
@@ -1,56 +1,56 @@
-'use strict';
+'use strict'
 
-var chai = require('chai');
-var assert = chai.assert;
+var chai = require('chai')
+var assert = chai.assert
 
-var checkClean = require('./fixtures').checkClean;
-var dockerMock = require('../../lib/index');
-var request = require('request');
+var checkClean = require('./fixtures').checkClean
+var dockerMock = require('../../lib/index')
+var request = require('request')
 
 var docker = require('dockerode')({
   host: 'http://localhost',
   port: 5354
-});
+})
 
 describe('docker misc', function () {
-  var server;
-  before(function (done) { server = dockerMock.listen(5354, done); });
+  var server
+  before(function (done) { server = dockerMock.listen(5354, done) })
   // make sure we are starting with a clean mock
   // (tests should clean-up after themselves)
-  beforeEach(function (done) { checkClean(docker, done); });
-  after(function (done) { server.close(done); });
+  beforeEach(function (done) { checkClean(docker, done) })
+  after(function (done) { server.close(done) })
 
   describe('/info', function () {
     it('should return info data', function (done) {
       docker.info(function (err, data) {
-        if (err) { return done(err); }
-        assert.propertyVal(data, 'Mock', true);
-        done();
-      });
-    });
-  });
+        if (err) { return done(err) }
+        assert.propertyVal(data, 'Mock', true)
+        done()
+      })
+    })
+  })
 
   describe('/version', function () {
     it('should return version data', function (done) {
       docker.version(function (err, data) {
-        if (err) { return done(err); }
-        assert.ok(data.Os);
-        done();
-      });
-    });
-  });
+        if (err) { return done(err) }
+        assert.ok(data.Os)
+        done()
+      })
+    })
+  })
 
   describe('invalid endpoints', function () {
     it('should respond with an error', function (done) {
       request.get('http://localhost:5354/_nope', function (err, res) {
         if (err) {
-          done(err);
+          done(err)
         } else if (res.statusCode !== 501) {
-          done('should have sent a 501 error');
+          done('should have sent a 501 error')
         } else {
-          done();
+          done()
         }
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
